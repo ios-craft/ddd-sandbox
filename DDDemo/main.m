4a1018c87772ab3809f12b762721a741c2d89b36
Tomek Cejner <tomek.cejner@gmail.com>
2014-10-29 22:44:09 +0100

    Initial Commit

---
+++ DDDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DDDemo
+//
+//  Created by Tomek Cejner on 29/10/14.
+//  Copyright (c) 2014 Tomek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
